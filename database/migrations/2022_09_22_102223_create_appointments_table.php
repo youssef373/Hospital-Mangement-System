a7763bd6a250e6712190ab1361c6974e35c68670
Youssef Mamdouh <65975984+youssef373@users.noreply.github.com>
2022-10-12 19:58:01 +0200

    Add files via upload

---
+++ database/migrations/2022_09_22_102223_create_appointments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('date')->nullable();
+            $table->string('doctor')->nullable();
+            $table->integer('phone')->nullable();
+            $table->string('message')->nullable();
+            $table->string('status')->default('in progress');
+            $table->integer('user_id')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
